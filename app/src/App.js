796681c0dbf504e0454efd7af170e36434bd13e7
Ville Kuosmanen <ville@villekuosmanen.com>
2024-01-28 05:21:33 +0000

    add app boilerplate and basic human svg

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import human from './human.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={human} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
